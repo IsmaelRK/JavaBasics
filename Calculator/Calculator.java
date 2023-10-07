fa5d37ed03cb70fdf2c027b3730812e3e956d604
Ismael <ismael2021ab@gmail.com>
2023-10-06 21:05:53 -0300

    Feat: Learning

---
+++ Calculator/Calculator.java
@@ -0,0 +1,19 @@
+package Calculator;
+import java.util.Scanner;
+
+public class Calculator {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter the Number: ");
+        int num = scan.nextInt();
+
+        System.out.print("The number is: " + num);
+
+    }
+
+
+
+}
