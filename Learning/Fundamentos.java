25d1da734c722e295e3201eb4ece3c9d446f85f1
Ismael <ismael2021ab@gmail.com>
2023-10-14 09:23:34 -0300

    feat: upd

---
+++ Learning/Fundamentos.java
@@ -0,0 +1,18 @@
+package Learning;
+
+public class Fundamentos {
+
+
+
+    public static void main(String[] args) {
+
+        int inteiro = 16;
+        double flutuante = 3.1415;
+        char caracter = 'I';
+        boolean falso = false;
+
+        System.out.println(inteiro);
+        System.out.println(falso);
+    }
+
+}
